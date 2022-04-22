bc104fdeb8cdb37d50333f50157609a5d220dc9e
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-22 14:52:20 -0500

    Change all

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,19 +4,29 @@
 
 def canUnlockAll(boxes):
     '''Function to check if all box can be open'''
-    open = {0: 'True'}
+    open = {0: True}
+
+    # for i in range(0, len(boxes)):
+    #     for j in range(0, len(boxes[i])):
+    #         key = boxes[i][j]
+    #         if key <= len(boxes) and i in open.keys():
+    #             open[key] = True
+
+    # for m in range(len(boxes)-1, -1, -1):
+    #     for n in range(0, len(boxes[m])):
+    #         key = boxes[m][n]
+    #         if key <= len(boxes) and i in open.keys():
+    #             open[key] = True
 
     for i in range(0, len(boxes)):
-        for j in range(0, len(boxes[i])):
-            key = boxes[i][j]
-            if key <= len(boxes) and i in open.keys():
-                open[key] = True
-
-    for m in range(len(boxes)-1, -1, -1):
-        for n in range(0, len(boxes[m])):
-            key = boxes[m][n]
-            if key <= len(boxes) and i in open.keys():
-                open[key] = True
+        if (len(boxes[i]) < len(boxes)):
+            for j in boxes[i]:
+                if j <= len(boxes) and i in open.keys():
+                    open[j] = True
+                    if (j < len(boxes)):
+                        for x in boxes[j]:
+                            if x <= len(boxes) and j in open.keys():
+                                open[x] = True
 
     if (len(open) == len(boxes)):
         return True
