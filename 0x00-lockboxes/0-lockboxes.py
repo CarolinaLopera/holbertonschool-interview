ff2c789aa5a0bad765eb432b8666c518b1a98701
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-21 23:19:49 -0500

    Two loops

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -12,18 +12,6 @@ def canUnlockAll(boxes):
             if key <= len(boxes) and i in open:
                 open[key] = True
 
-    for x in range(0, len(boxes)):
-        for y in range(0, len(boxes[x])):
-            key = boxes[x][y]
-            if key <= len(boxes) and i in open:
-                open[key] = True
-
-    for k in range(len(boxes)-1, -1, -1):
-        for l in range(0, len(boxes[k])):
-            key = boxes[k][l]
-            if key <= len(boxes) and i in open:
-                open[key] = True
-
     for m in range(len(boxes)-1, -1, -1):
         for n in range(0, len(boxes[m])):
             key = boxes[m][n]
