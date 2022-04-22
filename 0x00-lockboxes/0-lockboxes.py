fcb256122cae4f442aa523093ff62136a09214b6
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-21 23:40:00 -0500

    Fix index variable

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -15,7 +15,7 @@ def canUnlockAll(boxes):
     for m in range(len(boxes)-1, -1, -1):
         for n in range(0, len(boxes[m])):
             key = boxes[m][n]
-            if key <= len(boxes) and i in open:
+            if key <= len(boxes) and m in open:
                 open[key] = True
 
     if (len(open) == len(boxes)):
