50f554ef76eb808cf91d0fbe6969c0ea58c5aa9f
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-21 22:28:50 -0500

    Try

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''Question interview'''
+
+
+def canUnlockAll(boxes):
+    '''Function to check if all box can be open'''
+    open = {0: 'True'}
+
+    for i in range(0, len(boxes)):
+        for j in range(0, len(boxes[i])):
+            key = boxes[i][j]
+            if key <= len(boxes) and i in open:
+                open[key] = True
+
+    for x in range(0, len(boxes)):
+        for z in range(0, len(boxes[x])):
+            key = boxes[x][z]
+            if key <= len(boxes) and i in open:
+                open[key] = True
+
+    if (len(open) == len(boxes)):
+        return True
+
+    return False
+
+    # print('dict', open)
+    # print('i:', i); print('box[i]:', boxes[i]); print()
+    # print('j:', j); print('box[i][j]: ', boxes[i][j]); print()
+    # print('add:', key)
+    # print(len(boxes))
