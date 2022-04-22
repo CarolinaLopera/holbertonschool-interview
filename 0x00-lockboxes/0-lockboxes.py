44924e65699f64477b7ee779f6e7fd56de6c07f4
CarolinaLopera <carolinalopera08@gmail.com>
2022-04-22 00:10:26 -0500

    with i

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -12,10 +12,10 @@ def canUnlockAll(boxes):
             if key <= len(boxes) and i in open.keys():
                 open[key] = True
 
-    for m in range(0, len(boxes)):
+    for m in range(len(boxes)-1, -1, -1):
         for n in range(0, len(boxes[m])):
             key = boxes[m][n]
-            if key <= len(boxes) and m in open.keys():
+            if key <= len(boxes) and i in open.keys():
                 open[key] = True
 
     if (len(open) == len(boxes)):
