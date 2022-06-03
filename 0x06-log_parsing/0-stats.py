c9f1131d66c47b18ffb5640a560e526afef2dd70
CarolinaLopera <carolinalopera08@gmail.com>
2022-06-03 00:41:21 -0500

    D:

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -15,16 +15,16 @@ try:
         div = cpy.split()
         count += 1
         size += int(div[8])
-        for key in dict:
-            if div[7] == key:
-                dict[key] += 1
+        if div[7] in dict:
+            dict[div[7]] += 1
         if count == 10:
             print('File size:', size)
             count = 0
             for key in dict:
                 if dict[key] != 0:
                     print('{}: {}'.format(key, dict[key]))
-except KeyboardInterrupt:
+# except KeyboardInterrupt:
+finally:
     print('File size:', size)
     for key in dict:
         if dict[key] > 0:
