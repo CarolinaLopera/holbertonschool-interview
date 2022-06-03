6a5db43f0bfbf8968a647551b1d227906d20dfe0
CarolinaLopera <carolinalopera08@gmail.com>
2022-06-02 23:13:27 -0500

    First try

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import fileinput
+
+
+count = 0
+size = 0
+dict = {'200': 0, '301': 0, '400': 0,
+        '401': 0, '403': 0, '404': 0,
+        '405': 0, '500': 0}
+
+for line in fileinput.input():
+    cpy = ''
+    for i in range(len(line)):
+        cpy += line[i]
+    div = cpy.split()
+
+    count += 1
+    size += int(div[8])
+    for key in dict:
+        if div[7] == key:
+            dict[key] += 1
+
+    if count == 10:
+        print('File size:', size)
+        count = 0
+        for key in dict:
+            if dict[key] != 0:
+                print('{}: {}'.format(key, dict[key]))
