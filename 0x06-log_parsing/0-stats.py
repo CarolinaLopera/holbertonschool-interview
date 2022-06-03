5a5a3ac40f091190a7c7862aa8fc5422e0f16621
CarolinaLopera <carolinalopera08@gmail.com>
2022-06-02 23:52:38 -0500

    :(

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 '''script that reads stdin line by line and computes metrics'''
-
-
-import sys
+import fileinput
 
 
 count = 0
@@ -11,7 +9,7 @@ dict = {'200': 0, '301': 0, '400': 0, '401': 0,
         '403': 0, '404': 0, '405': 0, '500': 0}
 
 try:
-    for line in sys.stdin:
+    for line in fileinput.input():
         cpy = ''
         for i in range(len(line)):
             cpy += line[i]
