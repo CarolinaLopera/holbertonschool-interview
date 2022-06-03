5f8c2e60c98460e024324bf43b2d99bce4666617
CarolinaLopera <carolinalopera08@gmail.com>
2022-06-02 23:22:10 -0500

    add line

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,7 +2,6 @@
 '''Imports'''
 import fileinput
 
-
 count = 0
 size = 0
 dict = {'200': 0, '301': 0, '400': 0,
@@ -14,16 +13,15 @@ for line in fileinput.input():
     for i in range(len(line)):
         cpy += line[i]
     div = cpy.split()
-
     count += 1
     size += int(div[8])
     for key in dict:
         if div[7] == key:
             dict[key] += 1
-
     if count == 10:
         print('File size:', size)
         count = 0
         for key in dict:
             if dict[key] != 0:
                 print('{}: {}'.format(key, dict[key]))
+
